7d064009466552e60fb0d5d2a1177192ef6ee292
Mitalee <bimbette83@yahoo.com>
2018-10-01 22:24:55 +0530

    Force new connection for multiple disconnects

--- example/app.py
+++ example/app.py
@@ -32,81 +32,85 @@ def index():
     return render_template('index.html', async_mode=socketio.async_mode)
 
 
-@socketio.on('my_event', namespace='/test')
-def test_message(message):
+@socketio.on('web_event', namespace='/test_web')
+def test_web_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']})
+    print('in web_event')
+    emit('web_response',
+         {'data': message['data'], 'count': session['receive_count']}, namespace='/test_web')
 
-
-@socketio.on('my_broadcast_event', namespace='/test')
-def test_broadcast_message(message):
+@socketio.on('web_to_local_event', namespace='/test_web')
+def test_web_to_local_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         broadcast=True)
+    print('in web2local event')
+    emit('local_response',
+         {'data': message['data'], 'count': session['receive_count']}, namespace='/test_local')
 
 
-@socketio.on('join', namespace='/test')
-def join(message):
-    join_room(message['room'])
+@socketio.on('local_event', namespace='/test_local')
+def test_local_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
+    print('in local_event')
+    emit('local_response',
+         {'data': message['data'], 'count': session['receive_count']}, namespace='/test_local')
 
-
-@socketio.on('leave', namespace='/test')
-def leave(message):
-    leave_room(message['room'])
+@socketio.on('local_to_web_event', namespace='/test_local')
+def test_local_to_web_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': 'In rooms: ' + ', '.join(rooms()),
-          'count': session['receive_count']})
+    print('in local2web event')
+    emit('web_response',
+         {'data': message['data'], 'count': session['receive_count']}, namespace='/test_web')
 
 
-@socketio.on('close_room', namespace='/test')
-def close(message):
+@socketio.on('disconnect_request', namespace='/test_web')
+def web_disconnect_request():
     session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response', {'data': 'Room ' + message['room'] + ' is closing.',
-                         'count': session['receive_count']},
-         room=message['room'])
-    close_room(message['room'])
-
-
-@socketio.on('my_room_event', namespace='/test')
-def send_room_message(message):
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
-         {'data': message['data'], 'count': session['receive_count']},
-         room=message['room'])
-
-
-@socketio.on('disconnect_request', namespace='/test')
-def disconnect_request():
-    session['receive_count'] = session.get('receive_count', 0) + 1
-    emit('my_response',
+    emit('web_response',
          {'data': 'Disconnected!', 'count': session['receive_count']})
     disconnect()
 
+@socketio.on('disconnect_request', namespace='/test_local')
+def local_disconnect_request():
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('local_response',
+         {'data': 'Disconnected!', 'count': session['receive_count']})
+    disconnect()
 
-@socketio.on('my_ping', namespace='/test')
-def ping_pong():
-    emit('my_pong')
+@socketio.on('my_web_ping', namespace='/test_web')
+def ping_pong_web():
+    emit('my_web_pong')
+
+@socketio.on('my_local_ping', namespace='/test_local')
+def ping_pong_local():
+    emit('my_local_pong')
 
 
-@socketio.on('connect', namespace='/test')
+@socketio.on('connect', namespace='/test_web')
 def test_connect():
     global thread
     with thread_lock:
         if thread is None:
             thread = socketio.start_background_task(target=background_thread)
-    emit('my_response', {'data': 'Connected', 'count': 0})
+    emit('web_response', {'data': 'Connected', 'count': 0})
 
 
-@socketio.on('disconnect', namespace='/test')
-def test_disconnect():
+@socketio.on('disconnect', namespace='/test_web')
+def test_web_disconnect():
     print('Client disconnected', request.sid)
+
+@socketio.on('connect', namespace='/test_local')
+def test_local_connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
+    emit('local_response', {'data': 'Connected', 'count': 0})
+
+
+@socketio.on('disconnect', namespace='/test_local')
+def test_local_disconnect():
+    print('Client disconnected', request.sid)
+
 
 
 if __name__ == '__main__':

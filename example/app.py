97d39e0b60d5629a24b6f21d0874406c49a36dff
Mitalee <bimbette83@yahoo.com>
2018-10-02 23:55:23 +0530

    electron disconnect working

--- example/app.py
+++ example/app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python
 from threading import Lock
-from flask import Flask, render_template, session, request, jsonify
+from flask import Flask, render_template, session, request, jsonify, url_for
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 from flask_login import LoginManager, UserMixin, current_user, login_user, \
     logout_user
 
 from flask_session import Session
+
+from celery import Celery
+import time
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
 # the best option based on installed packages.
@@ -15,11 +18,20 @@ async_mode = None
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['SESSION_TYPE'] = 'filesystem'
-socketio = SocketIO(app, async_mode=async_mode)
+socketio = SocketIO(app, async_mode=async_mode, message_queue='redis://localhost:6379/0')
 login = LoginManager(app)
 Session(app)
 thread = None
 thread_lock = Lock()
+
+# Celery configuration
+app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+
+
+# Initialize Celery
+celery = Celery(app.name, broker=app.config.get('CELERY_BROKER_URL'))
+celery.conf.update(app.config)
 
 class User(UserMixin, object):
     def __init__(self, id=None):
@@ -92,7 +104,7 @@ def test_local_message(message):
     session['receive_count'] = session.get('receive_count', 0) + 1
     print('in local_event: ', message)
     emit('local_response',
-         {'data': message['data'], 'count': 'from_server'+session['receive_count']})
+         {'data': message['data'], 'count': 'from_server'+str(session['receive_count'])})
 
 @socketio.on('local_to_web_event', namespace='/test_local')
 def test_local_to_web_message(message):
@@ -112,14 +124,16 @@ def test_local_to_web_message(message):
 @socketio.on('disconnect_request', namespace='/test_local')
 def local_disconnect_request():
     session['receive_count'] = session.get('receive_count', 0) + 1
+    print('in disconnect_request')
     emit('local_response',
          {'data': 'Disconnected!', 'count': session['receive_count']})
+    print('emitted from disconnect')
     disconnect()
 
 
-# @socketio.on('disconnect', namespace='/test_local')
-# def test_local_disconnect():
-#     print('Client disconnected', request.sid)
+@socketio.on('disconnect', namespace='/test_local')
+def test_local_disconnect():
+    print('Client disconnected', request.sid)
 
 #To allow desktop local client to access the user session (currently by username)
 @socketio.on('join', namespace='/test_local')
@@ -130,6 +144,27 @@ def join(message):
     emit('local_response',
          {'data': 'In rooms: ' + ', '.join(rooms()),
           'count': session['receive_count']})
+
+@socketio.on('leave', namespace='/test_local')
+def leave(message):
+    leave_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('local_response',
+         {'data': 'In rooms: ' + ', '.join(rooms()),
+          'count': session['receive_count']})
+
+@celery.task(bind=False)
+def long_task():
+    for i in range(5):
+        d = 'number'+str(i)
+        socketio.emit('local_response',
+         {'data': d, 'count': '0901'}, namespace='/test_local', room='abc')
+        time.sleep(2)
+
+@app.route('/longtask', methods=['POST'])
+def longtask():
+    task = long_task.apply_async()
+    return jsonify({}), 202, {'Location':'xyz'}
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
